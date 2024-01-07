52681a43ffc2fe20c24fe8346db592e5f3b7f435
Arpan Das <dasarpan471@gmail.com>
2024-01-07 12:41:10 +0530

    initial commit

---
+++ linkedlist/intro.ts
@@ -0,0 +1,48 @@
+class node {
+    data: number;
+    next: node | null;
+
+    constructor(data: number) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    head: node | null;
+
+    constructor() {
+        this.head = null;
+    }
+
+    append(data: number): void {
+        const newNode = new node(data);
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+
+        current.next = newNode;
+    }
+
+    display(): void {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+// Example usage
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+
+linkedList.display();
