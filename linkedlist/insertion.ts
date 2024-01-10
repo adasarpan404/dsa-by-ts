e92785fefbb40b60d4c9bce20ea9048632de7989
Arpan Das <dasarpan471@gmail.com>
2024-01-10 05:36:04 +0530

    insertion code added

---
+++ linkedlist/insertion.ts
@@ -0,0 +1,79 @@
+class nodeinsertion {
+    data: number;
+    next: nodeinsertion | null;
+    constructor(data: number) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class InsertionLinkedList {
+    head: nodeinsertion | null;
+    constructor() {
+        this.head = null;
+    }
+    insertAtHead(data: number): void {
+        const newNode = new nodeinsertion(data);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
+    insertAtTail(data: number): void {
+        const newNode = new nodeinsertion(data);
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+    insertAfter(data: number, key: number): void {
+        const newNode = new nodeinsertion(data);
+        if (key == this.head!.data) {
+            newNode.next = this.head!.next;
+            this.head!.next = newNode;
+            return;
+        }
+        let temp = this.head;
+        while (temp!.data != key) {
+            temp = temp!.next;
+            if (temp == null) {
+                return;
+            }
+        }
+        newNode.next = temp!.next;
+        temp!.next = newNode
+    }
+    display(): void {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+const insertionLinkedList = new InsertionLinkedList();
+console.log("insertion at head")
+insertionLinkedList.insertAtHead(1);
+insertionLinkedList.insertAtHead(2);
+insertionLinkedList.insertAtHead(3);
+
+insertionLinkedList.display()
+
+// console.log("insertion at tail")
+// const insertionLinkedList1 = new InsertionLinkedList();
+// insertionLinkedList1.insertAtTail(1);
+// insertionLinkedList1.insertAtTail(2);
+// insertionLinkedList1.insertAtTail(3);
+// insertionLinkedList1.display()
+
+// console.log("inserting 4 after 2")
+
+// insertionLinkedList1.insertAfter(4, 2);
+// insertionLinkedList1.display();
+
+// 3-> 2-> 1
