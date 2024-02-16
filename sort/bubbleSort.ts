38b72b6ec4af5dab4be6b96921574a7116738fa1
Arpan Das <dasarpan471@gmail.com>
2024-02-16 07:17:33 +0530

    insertion sort and selection sort completed

---
+++ sort/bubbleSort.ts
@@ -0,0 +1,20 @@
+function bubbleSort(arr: number[]): number[] {
+    const length = arr.length
+
+    for (let i = 0; i < length - 1; i++) {
+        for (let j = 0; j < length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j + 1]
+                arr[j + 1] = arr[j]
+                arr[j] = temp
+            }
+        }
+    }
+    return arr;
+}
+
+let bubbleSortArray = [3, 4, 5, 1, 2]
+
+bubbleSortArray = bubbleSort(bubbleSortArray)
+
+console.log(bubbleSortArray)
