4b1a88ae4ec19ba0e7992d6b9d3fd38e322791c0
Arpan Das <dasarpan471@gmail.com>
2024-02-16 07:23:27 +0530

    selection sort completed in typescript

---
+++ sort/selectionSort.ts
@@ -0,0 +1,24 @@
+function selectionSort(arr: number[]): number[] {
+    const length = arr.length;
+    for (let i = 0; i < length - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            // Swap elements
+            const temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+    return arr;
+}
+
+let selectionSortArray = [3, 1, 4, 5, 2];
+
+selectionSortArray = selectionSort(selectionSortArray)
+
+console.log(selectionSortArray)
