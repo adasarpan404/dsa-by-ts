0c395e5ba2932c3fce704a618ee350e01d0628ca
Arpan Das <dasarpan471@gmail.com>
2024-02-18 08:32:25 +0530

    queue implemented done

---
+++ queue/index.ts
@@ -0,0 +1,57 @@
+class Queue<T> {
+    private items: T[]
+
+    constructor() {
+        this.items = []
+    }
+
+    enqueue(item: T): void {
+        this.items.push(item)
+    }
+
+    dequeue(): T | undefined {
+        return this.items.shift()
+    }
+
+    peek(): T | undefined {
+        return this.items[0];
+    }
+
+    isEmpty(): boolean {
+        return this.items.length === 0;
+    }
+
+    size(): number {
+        return this.items.length;
+    }
+
+    clear(): void {
+        this.items = [];
+    }
+}
+
+const queue = new Queue<number>()
+
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+
+console.log("Size:", queue.size())
+console.log("Peek:", queue.peek())
+console.log("Dequeue:", queue.dequeue())
+console.log("Size:", queue.size())
+queue.clear()
+console.log("Is empty:", queue.isEmpty())
+
+console.log("String Queue implemented")
+const stringQueue = new Queue<string>()
+
+stringQueue.enqueue("arpan")
+stringQueue.enqueue("das")
+
+console.log("Size:", stringQueue.size())
+console.log("Peek:", stringQueue.peek())
+console.log("Dequeue:", stringQueue.dequeue())
+console.log("Size:", stringQueue.size())
+stringQueue.clear()
+console.log("Is Empty:", stringQueue.isEmpty())
--- stack/index.ts
+++ stack/index.ts
@@ -1,4 +1,4 @@
-class Stack<T> {
+export class Stack<T> {
     private items: T[]
 
     constructor() {
@@ -30,29 +30,29 @@ class Stack<T> {
     }
 }
 
-const stack = new Stack<number>()
+// const stack = new Stack<number>()
 
-stack.push(1)
-stack.push(2)
-stack.push(3)
+// stack.push(1)
+// stack.push(2)
+// stack.push(3)
 
-console.log("Size:", stack.size())
-console.log("Peek:", stack.peek())
-console.log("Pop:", stack.pop())
-console.log("Size:", stack.size())
-stack.clear()
-console.log("Is empty:", stack.isEmpty())
+// console.log("Size:", stack.size())
+// console.log("Peek:", stack.peek())
+// console.log("Pop:", stack.pop())
+// console.log("Size:", stack.size())
+// stack.clear()
+// console.log("Is empty:", stack.isEmpty())
 
 
-console.log("example of string stack")
-const stringStack = new Stack<string>()
+// console.log("example of string stack")
+// const stringStack = new Stack<string>()
 
-stringStack.push("arpan")
-stringStack.push("das")
+// stringStack.push("arpan")
+// stringStack.push("das")
 
-console.log("Size:", stringStack.size())
-console.log("Peek:", stringStack.peek())
-console.log("Pop:", stringStack.pop())
-console.log("Size:", stringStack.size())
-stringStack.clear()
-console.log("Is empty:", stringStack.isEmpty())
+// console.log("Size:", stringStack.size())
+// console.log("Peek:", stringStack.peek())
+// console.log("Pop:", stringStack.pop())
+// console.log("Size:", stringStack.size())
+// stringStack.clear()
+// console.log("Is empty:", stringStack.isEmpty())
