a0773e8f59ba32671ff137f140671bd4e47dee64
Arpan Das <dasarpan471@gmail.com>
2024-02-24 20:12:16 +0530

    dnf sort completed

--- sort/index.ts
+++ sort/index.ts
@@ -1,4 +1,5 @@
 import { bubbleSort } from "./bubbleSort"
+import { dnfSort } from "./dnfSort"
 import { insertionSort } from "./insertionSort"
 import { recursiveBubbleSort } from "./recursiveBubbleSort"
 import { recursiveInsertionSort } from "./recursiveInsertionSort"
@@ -6,6 +7,8 @@ import { recursiveSelectionSort } from "./recursiveSelectionSort"
 import { selectionSort } from "./selectionSort"
 
 const arr = [5, 3, 8, 1, 2]
+
+const dnfArr = [0, 2, 1, 1, 0]
 
 let sortArr = bubbleSort(arr)
 
@@ -30,3 +33,7 @@ console.log("Sorting after recursive selection sort", sortArr)
 sortArr = recursiveInsertionSort(arr)
 
 console.log("Sorting after recursive insertion sort", sortArr)
+
+let sortDnfArr = dnfSort(dnfArr)
+
+console.log("Sorting after Dutch National Flag Sort", sortDnfArr)
--- sort/sort.test.ts
+++ sort/sort.test.ts
@@ -1,4 +1,5 @@
 import { bubbleSort } from './bubbleSort';
+import { dnfSort } from './dnfSort';
 import { insertionSort } from './insertionSort';
 import { recursiveBubbleSort } from './recursiveBubbleSort';
 import { recursiveInsertionSort } from './recursiveInsertionSort';
@@ -152,6 +153,32 @@ describe("recursiveInsertionSort", () => {
         const inputArray = [9, 8, 7, 6, 5, 4, 3, 2, 1];
         const sortedArray = recursiveInsertionSort(inputArray);
         expect(sortedArray).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    });
+
+    it('should handle an empty array', () => {
+        const inputArray: number[] = [];
+        const sortedArray = recursiveInsertionSort(inputArray);
+        expect(sortedArray).toEqual([]);
+    });
+})
+
+describe("Dutch National Flag Sort (dnf)", () => {
+    it('should sort an array of numbers in ascending order', () => {
+        const inputArray = [0, 1, 2, 1, 0]
+        const sortedArray = dnfSort(inputArray)
+        expect(sortedArray).toEqual([0, 0, 1, 1, 2])
+    })
+
+    it('should return the same array if it is already sorted', () => {
+        const inputArray = [0, 0, 1, 1, 2]
+        const sortedArray = dnfSort(inputArray)
+        expect(sortedArray).toEqual([0, 0, 1, 1, 2])
+    })
+
+    it('should correctly sort a large array', () => {
+        const inputArray = [0, 0, 1, 1, 0, 0, 2, 2, 1, 1];
+        const sortedArray = recursiveInsertionSort(inputArray);
+        expect(sortedArray).toEqual([0, 0, 0, 0, 1, 1, 1, 1, 2, 2]);
     });
 
     it('should handle an empty array', () => {
